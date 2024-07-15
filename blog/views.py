b9d00150871a8627ada346ac407c25d30e7791fc
Batuhan <batukhan209@gmail.com>
2024-07-15 15:44:55 +0300

    nnn

---
+++ blog/views.py
@@ -0,0 +1,110 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Author
+from .forms import CommentForm
+from django.views.generic import ListView
+from django.views import View
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+        
+
+class AllPostsView(ListView):
+    template_name = "blog/all_posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+class SinglePostView(View):
+    def is_read_later(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_read_later = post_id in stored_posts
+        else:
+            is_read_later = False
+
+        return is_read_later
+        
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "is_read_later": self.is_read_later(request, post.id)
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_read_later = post.id in stored_posts
+        else:
+            is_read_later = False
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            comment_form.save()
+            return HttpResponseRedirect(reverse("post-detail", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+            "is_read_later": self.is_read_later(request, post.id)
+        }
+
+        return render(request, "blog/post-detail.html", context)
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored_posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST.get("post_id"), False)
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+
+        return HttpResponseRedirect("/")
